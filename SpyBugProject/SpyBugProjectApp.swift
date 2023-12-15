7fd52d5730596e1ba7cf9ce92fda935830a79dae
Pavel Kurzo <pavelkurzo.ios@gmail.com>
2023-12-15 13:06:41 +0100

    Drop files from .gitignore

---
+++ SpyBugProject/SpyBugProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpyBugProjectApp.swift
+//  SpyBugProject
+//
+//  Created by Pavel Kurzo on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpyBugProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
